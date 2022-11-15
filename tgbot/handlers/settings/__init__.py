31304a5010be3b42626539aead39c3bf7fa2ed3f
Radzih Vadim <iphonevadim2003@gmail.com>
2022-11-15 16:41:52 +0200

    updated

--- tgbot/handlers/settings/__init__.py
+++ tgbot/handlers/settings/__init__.py
@@ -3,9 +3,11 @@ from aiogram.dispatcher import Dispatcher
 from .start import register_start_handlers
 from .language import register_language_handlers
 from .notifications import register_notifications_handlers
+from .menu import register_menu_handlers
 
 
 def register_settings_handlers(dp: Dispatcher):
     register_start_handlers(dp)
     register_language_handlers(dp)
     register_notifications_handlers(dp)
+    register_menu_handlers(dp)
---
+++ tgbot/handlers/settings/menu.py
@@ -0,0 +1,23 @@
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import CallbackQuery
+from aiogram.contrib.middlewares.i18n import I18nMiddleware
+
+from tgbot.keyboards import inline
+
+
+async def go_to_menu(
+    call: CallbackQuery,
+    i18n: I18nMiddleware,
+):
+    await call.answer()
+    await call.message.delete()
+    await call.message.answer(
+        text=i18n.gettext('Щоб продовжити роботу з ботом, поверніться до головного меню'),
+        reply_markup=inline.go_to_menu_cross_button_markup
+    )
+
+
+def register_menu_handlers(dp: Dispatcher):
+    dp.register_callback_query_handler(
+        go_to_menu,
+    )
