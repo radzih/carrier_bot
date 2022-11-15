d91bebf63d1db5559fb63b73c71389e124f61eeb
Ubuntu <ubuntu@ip-172-31-86-126.ec2.internal>
2022-11-15 12:06:03 +0000

    Configured for production

--- web/web/urls.py
+++ web/web/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.conf.urls.static import serve
+
+from . import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve ,{'document_root': settings.STATIC_ROOT}),
 
 ]
