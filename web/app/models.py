e0863dd756bd3aae3630339a082e18fc1ade4c75
Radzih Vadim <iphonevadim2003@gmail.com>
2022-11-25 11:55:51 +0200

    Display route departure_time in another format

--- web/app/models.py
+++ web/app/models.py
@@ -248,7 +248,7 @@ class Route(models.Model):
         ).order_by('station_index').first()
         departure_time = '00'
         if first_station:
-            departure_time = first_station.departure_time.strftime('%d.%m')
+            departure_time = first_station.departure_time.strftime('%d.%m %H:%M')
         return departure_time
     departure_time.short_description = 'Час відправлення'
  
