68bf12a8dcac76866cce23fa1e32f0e34b08c826
Vadim Radzih <iphonevadim2003@gmail.com>
2022-12-21 15:01:53 +0200

    Remove dublicates when select station

--- tgbot/handlers/send_package/end_station.py
+++ tgbot/handlers/send_package/end_station.py
@@ -42,7 +42,9 @@ async def enter_end_station_callback(
     )
     popular_stations = await db.get_popular_stations(call.from_user.id)
 
-    stations = list(set(user_station_history[:2] + popular_stations))
+    stations_t = list(set(user_station_history[:2] + popular_stations))
+    stations = [s for s in stations_t if s not in stations]
+ 
     stations = [station for station in stations if station.id != start_station.id]
  
     await call.message.answer(
